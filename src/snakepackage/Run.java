fb660345e711366dc2494784416dc6cd377d5807
Ludwigninn <ludwigninn@hotmail.com>
2016-10-24 13:46:23 +0200

    init

---
+++ src/snakepackage/Run.java
@@ -0,0 +1,11 @@
+package snakepackage;
+
+public class Run {
+	
+	public static void main(String[] args){		
+		Exempel ex = new Exempel();
+		String path = "src/Map.txt";
+		ex.parseTextFile(path);
+	}
+
+}
